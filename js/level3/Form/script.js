3e50c025a332689dd07ac5d6158ea2d2539c4c3a
harimad <leesh68481@gmail.com>
2021-12-02 22:18:06 +0900

    interactive form 3: forEach loop

--- js/level3/Form/script.js
+++ js/level3/Form/script.js
@@ -1,9 +1,18 @@
 // 셔츠 선택하면 밑 UI를 보여줌
+//HTML을 자바스크립트로 짜서넣기(동적생성)
 
 $('#option1').on('change', () => {
-	if ($('#option1').val() == '셔츠') {
-		$('.size-select').show();
+	var 사이즈 = [26, 28, 30, 32, 34, 36];
+	var 템플릿 = '';
+
+	if ($('#option1').val() == '바지') {
+		$('#option2 option').remove();
+
+		사이즈.forEach((elem) => {
+			템플릿 += `<option>바지 : ${elem}</option>`;
+		})
+		$('#option2').append(템플릿);
 	} else {
-		$('.size-select').hide();
+		$('#option2 option').html('');
 	}
 });
