182c9565fae79b7fe2cc080dd7ba2b56561515d2
harimad <leesh68481@gmail.com>
2021-12-02 19:28:20 +0900

    tap - event bubbling advanced & dataset

--- js/level2/bootstrap/tap.js
+++ js/level2/bootstrap/tap.js
@@ -3,13 +3,29 @@
 // 1. 버튼0이 주황색으로 하이라이트
 // 2. 내용0이 보여야함
 
-let tabBtnLen = $('.tab-button').length;
+// let tabBtnLen = $('.tab-button').length;
 
-for (let i = 0; i < tabBtnLen; i++) {
-	$('.tab-button').eq(i).click(() => {
-		$('.tab-button').removeClass('active');
-		$('.tab-content').removeClass('show');
-		$('.tab-button').eq(i).addClass('active');
-		$('.tab-content').eq(i).addClass('show');
-	})
+// for (let i = 0; i < tabBtnLen; i++) {
+// 	$('.tab-button').eq(i).click(() => {
+// 		$('.tab-button').removeClass('active');
+// 		$('.tab-content').removeClass('show');
+// 		$('.tab-button').eq(i).addClass('active');
+// 		$('.tab-content').eq(i).addClass('show');
+// 	})
+// }
+
+// 탭기능 다시만들기: 이벤트 버블링 응용과 dataset
+function 탭열기(숫자) {
+	$('.tab-button').removeClass('active');
+	$('.tab-content').removeClass('show');
+	$('.tab-button').eq(숫자).addClass('active');
+	$('.tab-content').eq(숫자).addClass('show');
 }
+
+$('.list').click((e) => {
+	탭열기(e.target.dataset.id); // 탭열기(내가누른버튼에숨겨진숫자)
+});
+
+// jQuery 버전 , js - dataset와 같음
+$('.list').data('작명', '값'); //값설정
+$('.list').data('작명'); //값쓸때
