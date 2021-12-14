8a8f15498f3cfd7fcd4fc26f8578cb8e7498b9f2
harimad <leesh68481@gmail.com>
2021-12-14 17:14:42 +0900

    shop project Bootstrap connect

--- react/level1/shop/src/App.js
+++ react/level1/shop/src/App.js
@@ -1,9 +1,10 @@
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <button type="button" class="btn btn-primary">Primary</button>
     </div>
   );
 }
