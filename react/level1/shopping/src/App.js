95f436abf57c4c06a2ea4ebbf8504b9b97835a22
harimad <leesh68481@gmail.com>
2021-12-14 18:40:33 +0900

    shopping mall layout design

--- react/level1/shopping/src/App.js
+++ react/level1/shopping/src/App.js
@@ -1,12 +1,13 @@
-import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
       <Navbar bg="light" expand="lg">
         <Container fluid>
-          <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+          <Navbar.Brand href="#">Shoes Shop</Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
@@ -40,6 +41,40 @@ function App() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      {/* Jumbotron */}
+      <div className='jumbotron background'>
+        <h1>Hello, world!</h1>
+        <p>
+          This is a simple hero unit, a simple jumbotron-style component for calling
+          extra attention to featured content or information.
+        </p>
+        <p>
+          <Button variant="primary">Learn more</Button>
+        </p>
+      </div>
+
+      {/* Item Layout */}
+      <div className='container'>
+        <div className='row'>
+          <div className='col-md-4'>
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width='100%'></img>
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className='col-md-4'>
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width='100%'></img>
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className='col-md-4'>
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width='100%'></img>
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
